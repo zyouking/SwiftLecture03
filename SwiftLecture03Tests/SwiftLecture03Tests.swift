c5fb7318e5c460218f462cf08253c207756ab94c
zyouking <zyouqing@qq.com>
2018-07-22 17:09:13 +0800

    intital project

---
+++ SwiftLecture03Tests/SwiftLecture03Tests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftLecture03Tests.swift
+//  SwiftLecture03Tests
+//
+//  Created by stu1 on 2018/7/22.
+//  Copyright © 2018年 stu1. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftLecture03
+
+class SwiftLecture03Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
