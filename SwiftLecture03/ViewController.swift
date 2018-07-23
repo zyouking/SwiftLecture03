371ae8d2062bb33c45ea59967e81acbf76bc54fc
zyouking <zyouqing@qq.com>
2018-07-23 10:18:58 +0800

    btn click

--- SwiftLecture03/ViewController.swift
+++ SwiftLecture03/ViewController.swift
@@ -10,13 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var lbName:UILabel!
+    @IBOutlet weak var lbName: UILabel!
+    @IBOutlet weak var btnName: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func btnConfirmClicked(_ sender: Any) {
+        lbName.text="zyouking"
+    }
+    
+    
 }
 
